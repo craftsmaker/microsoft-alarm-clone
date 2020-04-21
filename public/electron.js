51a92692187568795e32b9bf35ca9337fab01c1f
Victor Guedes <victorguedes68@hotmail.com>
2020-04-21 14:42:58 -0300

    Boilerplate

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const {BrowserWindow,App} = require("electron");
+
+let mainWindow;
+function createWindow() {
+    mainWindow = new BrowserWindow({ 
+        width: 1200, 
+        height: 800,
+        icon: ""
+    });
+     
+    const path = require("path");
+    const isDev = require("electron-is-dev");
+
+    mainWindow.loadURL(
+        isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+    mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+    app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (mainWindow === null) {
+    createWindow();
+    }
+});
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Alarm APP made from zero</p>
+    </div>
+  );
+}
+
+export default App;
