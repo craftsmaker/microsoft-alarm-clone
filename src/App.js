a759f288f34a7c9526965cb142ea97f02720eb97
Victor Guedes <victorguedes68@hotmail.com>
2020-04-22 12:03:57 -0300

    Buttons aproximation

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ function Menu(){
 			</div>
 			<div>
 				<ul>
-					<li><button className="button">B1</button></li>
-					<li><button className="button">B2</button></li>
-					<li><button className="button">B3</button></li>
-					<li><button className="button">B4</button></li>
+					<li><button className="button">Alarme</button></li>
+					<li><button className="button">Temporizador</button></li>
+					<li><button className="button">Relógio</button></li>
+					<li><button className="button">Cronômetro</button></li>
 				</ul>
 			</div>
 		</header>
@@ -40,10 +40,10 @@ function Footer(){
 	return (
 		<footer>
 			<ul>
-				<li><button className="button">B1</button></li>
-				<li><button className="button">B2</button></li>
-				<li><button className="button">B3</button></li>
-				<li><button className="button">B4</button></li>
+				<li><button className="button">+</button></li>
+				<li><button className="button">ST</button></li>
+				<li><button className="button">Pin</button></li>
+				<li><button className="button">...</button></li>
 			</ul>
 		</footer>
 	)
