3fb1c626b01599d51f14572aff0f4040a51fa9c3
Victor Guedes <victorguedes68@hotmail.com>
2020-04-24 11:43:52 -0300

    Function to create a Clock added

--- src/ClockMenu.js
+++ src/ClockMenu.js
@@ -1,15 +1,41 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 export default function ClockMenu() {
+	const [clocks,setClocks] = useState([]);
+
+	useEffect(() => {
+		if (clocks){
+			let arrayO = []
+			for (let i = 1; i < localStorage.length+1 ; i++){
+				arrayO.push(JSON.parse(localStorage[i]))
+			}
+
+			setClocks(arrayO);
+		}
+	},[])
+
+	function handleClick(){
+		const object = {
+			id: (clocks.length + 1).toString(),
+			timer: parseInt((Math.random() * 100).toString(),10)
+		}
+
+		setClocks(clocks.concat(object))
+		localStorage.setItem(object.id, JSON.stringify(object));
+	}
+
 	return (
-		<div>
-			<Clock/>
+		<div className="clocks">
+			{clocks.map(clock => (
+				<Clock timer={clock.timer}/>
+			))}
+			<button onClick={handleClick}>Criar</button>
 		</div>
 	);
 }
 
-function Clock(){
-	const [timer,setTimer] = useState(1) // minutes
+function Clock(props){
+	const timer = props.timer // minutes
 
 	function handlePlay(){
 
@@ -24,7 +50,7 @@ function Clock(){
 	}
 
 	return(
-		<div>
+		<div className="clock">
 			<p>{timer}</p>
 			<ul>
 				<li>Reboot</li>
