22ec33464d4cb3ec921eb03fdd920c8ae91cc0b6
Victor Guedes <victorguedes68@hotmail.com>
2020-05-13 11:30:47 -0300

    Stopwatch basic funcionality added

--- src/screens/Stopwatch/index.js
+++ src/screens/Stopwatch/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import {FaPlay} from "react-icons/fa";
@@ -8,10 +8,55 @@ import {AiFillFlag} from "react-icons/ai";
 import "./styles.css";
 
 export default function Clock(){
-	const [time,setTime] = useState({hour:0,minute:0,second:"00"});
+	const [time,setTime] = useState({hour:"00",minute:"00",second:"00",microsecond: "00"});
+	const [isEnabled,setIsEnabled] = useState(false);
 
-	const handlePlay = (e) => {
-		console.log(e)
+	const parseNumberAndString = (string) => {
+		let newNumber = parseInt(string);
+		newNumber++;
+		let newString = "00";
+		if (newNumber < 10){
+			newString = "0" + String(newNumber);
+		}
+		else {
+			newString = String(newNumber);
+		}
+
+		return [newNumber,newString];
+	}
+
+	useEffect(() => {
+		if (isEnabled) {
+			setTimeout(() => {
+				const [intMicrosecond,stringMicrosecond] = parseNumberAndString(time.microsecond);
+
+				if (intMicrosecond === 100){
+					const [intSecond,stringSecond] = parseNumberAndString(time.second);
+
+					if (intSecond === 60){
+						const [intMinute,stringMinute] = parseNumberAndString(time.minute);
+
+						setTime({...time, minute: stringMinute,second: "00", microsecond: "00"})
+					}
+					else{
+						setTime({...time, second: stringSecond, microsecond: "00"})	
+					}
+					
+				}
+				else{
+					setTime({...time, microsecond: stringMicrosecond})
+				}
+				
+			}, 10)
+		}
+	}, [isEnabled,time])
+
+	const handlePlay = () => {
+		setIsEnabled(true);
+	}
+
+	const stopPlay = () => {
+		setIsEnabled(false);
 	}
 
 	return(
@@ -21,9 +66,9 @@ export default function Clock(){
 				<div id="stopwatch-container">
 					<div id="stopwatch-selection">
 						<div id="stopwatch">
-							<h1>{time.hour}:{time.minute}:{time.second}</h1>
+							<h1>{time.hour}:{time.minute}:{time.second}<p>,{time.microsecond}</p></h1>
 							<div id="stopwatch-buttons">
-								<IoMdRefresh/>
+								<IoMdRefresh onClick={stopPlay}/>
 								<FaPlay size={30} onClick={handlePlay}/>
 								<IoMdResize/>
 							</div>
